9ce3fd639f71b43dbd4c2b6c02af7d134598ad6b
Max Zinchenko <zzeenj@gmail.com>
2022-01-12 18:26:35 +0200

    disabled sandbox back for live

--- src/store/BankAccounts/saga.ts
+++ src/store/BankAccounts/saga.ts
@@ -35,8 +35,7 @@ function *createAgreementLink({ payload }: CreateAgreementLink) {
     method: SocketEndpoint.CREATE_BANK_AGREEMENT_LINK,
     api: ACCOUNT_API,
     messageId: `${Date.now()}`,
-    // args: { ...payload, sandbox: !LIVE, redirectUrl: window.location.origin }
-    args: { ...payload, sandbox: true, redirectUrl: window.location.origin }
+    args: { ...payload, sandbox: !LIVE, redirectUrl: window.location.origin }
   }));
 }
 
